a5ec895ca524223414d0af62eb3cffbfdd5b0650
Fernando Gonzalez <fgonz059@wch136-24.cs.ucr.edu>
2020-10-25 17:41:49 -0700

    Finished part 1

--- source/main.c
+++ source/main.c
@@ -1,7 +1,7 @@
-/*	Author: fgonz059
+/*	A://github.com/fernando3197/Lab4_sm.gitthor: fgonz059
  *  Partner(s) Name: 
- *	Lab Section:
- *	Assignment: Lab #  Exercise #
+ *	Lab Section: 22
+ *	Assignment: Lab # 4 Exercise # 1
  *	Exercise Description: [optional - include for your own benefit]
  *
  *	I acknowledge all content contained herein, excluding template or example
@@ -12,12 +12,63 @@
 #include "simAVRHeader.h"
 #endif
 
+enum States { Start, S0, S1, S2, S3 } state;
+unsigned char tempA;
+unsigned char tempB;
+
+void Tick(){
+	
+	unsigned char A0 = PINA & 0x01;
+	switch(state){
+		case Start:
+			state = S0;
+			break;
+		case S0:
+			state = A0 ? S1 : S0;
+			break;
+		case S1:
+			state = !A0 ? S2 : S1;
+			break;
+		case S2:
+			state = A0 ? S3 : S2;
+			break;
+		case S3:
+			state = !A0 ? S0 : S3;
+			break;
+		default:
+			break;			
+		}			
+	} // Transitions
+	
+	switch(state){
+		case S0:
+			break;
+		case S1:
+			tempB = 0x02;
+			break;
+		case S2:
+			break;
+		case S3:
+			B = 0x01;
+			break;
+		default:
+			break;
+		
+	} // State actions
+}
+
 int main(void) {
     /* Insert DDR and PORT initializations */
-
-    /* Insert your solution below */
-    while (1) {
-
+	PINA = 0x00; PORTA = 0xFF;
+	PINB = 0xFF; PORTB = 0x00;
+	
+ 	B = 0x01; // Initialize outputs
+	state = Start;
+	
+	while(1){
+		tempA = PORTA;
+		Tick();
+	}       
     }
     return 1;
 }
--- test/tests.gdb
+++ test/tests.gdb
@@ -26,19 +26,52 @@
 echo ======================================================\n
 echo Running all tests..."\n\n
 
-# Example test:
-test "PINA: 0x00, PINB: 0x00 => PORTC: 0"
-# Set inputs
-setPINA 0x00
-setPINB 0x00
-# Continue for several ticks
-continue 2
-# Set expect values
-expectPORTC 0
-# Check pass/fail
-checkResult
 
 # Add tests below
+
+test "PINA: 0x01, 0x00, 0x01, 0x00 => PORTB: 0x01"
+set state = S0
+setPINA 0x01
+continue 2
+setPINA  0x00
+continue 2
+setPINA 0x01
+continue 2
+setPINA 0x00
+continue 2
+expectPORTB 0x01 
+expect state S0
+checkResult 
+
+test "PINA: 0x01, 0x00, 0x01 => PORTB: 0x01"
+set state = S0
+setPINA 0x01
+continue 2
+setPINA 0x00
+continue 2
+setPINA 0x01
+continue 2
+expectPORTB 0x01
+expect state S3
+checkResult
+
+test "PINA: 0x01, 0x00 => PORTB: 0x02"
+set state = S0
+setPINA 0x01
+continue 2
+setPINA 0x00
+continue 2
+expectPORTB 0x02
+expect state S2
+checkResult
+
+test "PINA: 0x01 => PORTB: 0x02"
+set state = S0
+setPINA 0x01
+continue 2
+expectPORTB 0x02
+expect state S1
+checkResult
 
 # Report on how many tests passed/tests ran
 set $passed=$tests-$failed
